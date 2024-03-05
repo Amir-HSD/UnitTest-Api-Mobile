981ecfb1775b996081e4e0a7587cfbc63b7acbd9
AmirHSD <mosavian57@gmail.com>
2024-03-05 19:38:38 +0330

    Add project files.

---
+++ UnitTest-Api-Mobile/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest-Api-Mobile")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest-Api-Mobile")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3fb9bc06-cfd0-40a2-aa1e-f15ec8fa6668")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest-Api-Mobile/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace UnitTest_Api_Mobile
+{
+
+    [TestClass]
+    public class ApiTest
+    {
+
+        private HttpClient HC;
+
+        [TestInitialize]
+        public void Initialization()
+        {
+            HC = new HttpClient();
+        }
+        [TestCleanup]
+        public void Dispose()
+        {
+            HC.Dispose();
+        }
+
+        [TestMethod]
+        public async Task Test_Register()
+        {
+
+            var Response = HC.GetAsync("https://jsonplaceholder.typicode.com/posts").Result;
+
+            Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+            var json = Response.Content.ReadAsStringAsync().Result;
+
+            List<Posts> ResponseContent = JsonConvert.DeserializeObject<List<Posts>>(json);
+            Console.WriteLine($"StatusCode: {Response.StatusCode}");
+            Console.WriteLine($"Content: {ResponseContent.Count}");
+        }
+
+    }
+
+    public class Posts
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
+}
